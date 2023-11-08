8903e6e9ca3bb5a65830a1a8473d774e560eaebb
d.popov <dmitrypopof@mail.ru>
2023-11-08 21:49:19 +0300

    добавил selenide 6.19.1

---
+++ src/test/java/com/swag_labs/Test_std_Users.java
@@ -0,0 +1,16 @@
+package com.swag_labs;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class Test_std_Users {
+    @Test
+    public void openPageStart(){
+        open("https://www.saucedemo.com/");
+
+
+    }
+}
